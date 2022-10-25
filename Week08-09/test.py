3d613049daff7ad6e5f24eed4f4a4dc8ceaac736
Prakrati-Magnani <54871651+Prakrati-Magnani@users.noreply.github.com>
2022-10-25 17:33:24 +1100

    yolo5

--- Week08-09/test.py
+++ Week08-09/test.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 # Model
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 print(model)
@@ -7,6 +8,6 @@ img = '/mnt/c/Users/prakr/Documents/GitHub/ECE4078_Lab_Group_112/Week08-09/test2
 # Inference
 results = model(img)
 # Results
-results.print()
-results.show()
+#results.print()
+#results.show()
 print(results.xyxy[0].np())
