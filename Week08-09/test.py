255a8db9de428cfe830e1fe3b314cc7f484b5c82
Prakrati-Magnani <54871651+Prakrati-Magnani@users.noreply.github.com>
2022-10-25 17:34:09 +1100

    yolo5

--- Week08-09/test.py
+++ Week08-09/test.py
@@ -4,7 +4,7 @@ import numpy as np
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 print(model)
 # Images
-img = '/mnt/c/Users/prakr/Documents/GitHub/ECE4078_Lab_Group_112/Week08-09/test2.jpg'  # image path
+img = '/mnt/c/Users/prakr/Documents/GitHub/ECE4078_Lab_Group_112/Week08-09/test1.jpg'  # image path
 # Inference
 results = model(img)
 # Results
