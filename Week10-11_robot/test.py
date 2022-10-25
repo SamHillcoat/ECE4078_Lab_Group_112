d5374c712af58fcd8994fa5e9d433394e6c3adad
Prakrati-Magnani <54871651+Prakrati-Magnani@users.noreply.github.com>
2022-10-25 20:38:51 +1100

    yolo5 for robot

--- Week10-11_robot/test.py
+++ Week10-11_robot/test.py
@@ -2,7 +2,7 @@ import torch
 import numpy
 
 # Model
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5s.pt')  # local model
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5s.pt',force_reload=True)  # local model
 #print(model)
 # Images
 img = '/mnt/c/Users/prakr/Documents/GitHub/ECE4078_Lab_Group_112/Week08-09/lemon17.jpg'  # image path
